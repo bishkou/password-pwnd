dc5a59efe5b50114e55c79310eb00e6858b6f5f0
bishkou <chedyhammami@yahoo.com>
2021-02-05 18:12:20 +0100

    check setup

---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+
+console.log('aaaaaaaaaaaa')
+// YOU SHOULD ADD THE STENGHT FEATURE
+
+const pwnd = async (password) => {
+    password = password.trim()
+    const hashed_pwd = await crypto.createHash('sha1').update(password).digest('hex');
+
+    const head = hashed_pwd.slice(0, 5);
+    const tail = hashed_pwd.slice(5).toUpperCase();
+    const url = 'https://api.pwnedpasswords.com/range/' + head
+
+    const data = await fetch(url)
+        .then(res => res.text())
+        .catch( () => {
+            return Promise.reject('Failed to fetch HIBP API')
+        })
+    if (data) {
+        const re = new RegExp(tail + ':(\\d+)');
+        const match = data.match(re);
+        if (match) {
+            return parseInt(match[1])
+        }
+        return 0
+    }
+    return Promise.reject('Something went wrong')
+
+}
+
+
+// check("password").then(data => {
+//     console.log(data)
+// })
+
+module.exports = {
+    pwnd: pwnd
+}
+
+// YOU SHOULD ADD THE STENGHT FEATURE
